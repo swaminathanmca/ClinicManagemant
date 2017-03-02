c9f43e83104134df3b7c3d01bf1ff18f8efb255f
jayaswaminathan <swaminathan@lissomsoft.com>
2017-03-02 10:11:42 +0530

    Add Clinic Changes Commit

---
+++ src/main/java/com/lissomsoft/clinic/rowmapper/ProfileMapper.java
@@ -0,0 +1,23 @@
+package com.lissomsoft.clinic.rowmapper;
+
+import com.lissomsoft.clinic.domain.Profile;
+
+
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Admin on 3/1/2017.
+ */
+public class ProfileMapper implements RowMapper<Profile> {
+
+    public Profile mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        Profile profile=new Profile();
+        profile.setProfile_id(resultSet.getInt("profile_id"));
+        profile.setName(resultSet.getString("name"));
+
+        return profile;
+    }
+}
