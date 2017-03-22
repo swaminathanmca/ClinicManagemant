891bd3fbc0b4ddbc5c543f1eba396a4921c979e5
swaminathanmca <swaminathan@lissomsoft.com>
2017-03-22 16:44:01 +0530

    Add Doctor Commit   22/03/2017

---
+++ src/main/java/com/lissomsoft/clinic/domain/Doctor.java
@@ -0,0 +1,108 @@
+package com.lissomsoft.clinic.domain;
+
+/**
+ * Created by Lissomsoft on 3/20/2017.
+ */
+public class Doctor {
+
+    private  int doctor_id;
+    private int user_id ;
+    private int clinic_id;
+    private int branch_id;
+    private  int reg_id;
+    private String qualification;
+    private String specialization;
+    private String created_at;
+    private  String updated_at;
+    private String created_by;
+    private String updated_by;
+
+
+    public int getDoctor_id() {
+        return doctor_id;
+    }
+
+    public void setDoctor_id(int doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getClinic_id() {
+        return clinic_id;
+    }
+
+    public void setClinic_id(int clinic_id) {
+        this.clinic_id = clinic_id;
+    }
+
+    public int getBranch_id() {
+        return branch_id;
+    }
+
+    public void setBranch_id(int branch_id) {
+        this.branch_id = branch_id;
+    }
+
+    public int getReg_id() {
+        return reg_id;
+    }
+
+    public void setReg_id(int reg_id) {
+        this.reg_id = reg_id;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getCreated_by() {
+        return created_by;
+    }
+
+    public void setCreated_by(String created_by) {
+        this.created_by = created_by;
+    }
+
+    public String getUpdated_by() {
+        return updated_by;
+    }
+
+    public void setUpdated_by(String updated_by) {
+        this.updated_by = updated_by;
+    }
+}
