e32bd3f3064d2c14fa34de7501c4302ecc0842f5
lissomsoftteam <devteam@lissomsoft.com>
2017-04-17 11:51:15 +0530

    Doctor Dashboard by Naveen kumar

---
+++ src/main/webapp/static/js/showPatient.js
@@ -0,0 +1,32 @@
+app.controller('showpatient',function($scope,$http,$window){
+    $scope.role=$window.sessionStorage.role_name;
+    $scope.email= $window.sessionStorage.email;
+
+
+
+    $http.get("trackSessionBranch/" + $scope.email).
+        then(function (response, status, headers, config) {
+
+            $window.sessionStorage.clinic_id=response.data.clinic_id;
+            $window.sessionStorage.doctor_id=response.data.doctor_id;
+            $scope.clinic_id=response.data.clinic_id;
+            $scope.data = response.data;
+        });
+
+    $http.get("ViewBranch/"+ $window.sessionStorage.clinic_id)
+        .then(function (response){
+            $scope.branchDetails=response.data.branch;
+
+
+        });
+
+
+    $scope.doctor=function(id){
+        $scope.branch_id=id;
+        $http.get("ShowPatient/"+$scope.branch_id+"/"+$window.sessionStorage.doctor_id)
+
+            .then(function (response){
+                $scope.patientDetails=response.data.patient;
+            })
+    }
+})
