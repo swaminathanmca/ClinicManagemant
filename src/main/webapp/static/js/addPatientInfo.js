dae59b5426327371aca61fdb9fdf66f1ddbea810
swaminathanmca <swaminathan@lissomsoft.com>
2017-04-20 15:31:10 +0530

    Delete addPatientInfo.js

--- src/main/webapp/static/js/addPatientInfo.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Admin on 4/19/2017.
- */
-app.controller('GetComplaint',function($scope,$http,$window){
-    $scope.role = $window.sessionStorage.role_name;
-    $scope.email = $window.sessionStorage.email;
-
-})
