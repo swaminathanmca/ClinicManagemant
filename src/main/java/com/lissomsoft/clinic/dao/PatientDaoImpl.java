3540e8f1a99d953f9b980daee178c7a870ec722b
swaminathanmca <swaminathan@lissomsoft.com>
2017-06-03 10:33:48 +0530

    PatientReg Commit

--- src/main/java/com/lissomsoft/clinic/dao/PatientDaoImpl.java
+++ src/main/java/com/lissomsoft/clinic/dao/PatientDaoImpl.java
@@ -309,7 +309,7 @@ if((result >0 )? true :false){
             String getPatientVisitSql="SELECT  pm.visit_id,p.first_name,p.last_name,pm.branch_id,p.patient_pid,pm.doctor_detail_id,pm.entry_time,pm.visit_type,pm.refereal_details,pm.type FROM patient_master p INNER JOIN patient_visit pm ON p.patient_id=pm.patient_id AND pm.status=1 AND pm.branch_id=:branch_id AND pm.created_at=:date";
             Map<String,Object> parameter=new HashMap<String, Object>();
             parameter.put("branch_id",branch_id);
-            parameter.put("date",format.format(new Date()));
+            parameter.put("date",dateFormat.format(new Date()));
             patientVisit=jdbcTemplate.query(getPatientVisitSql,parameter,new PatientVisitMapper());
         }catch (Exception e) {
             e.printStackTrace();
@@ -326,7 +326,7 @@ if((result >0 )? true :false){
             Map<String,Object> parameter=new HashMap<String, Object>();
             parameter.put("doctor_id",doctor_id);
             parameter.put("branch_id",branch_id);
-            parameter.put("date",format.format(new Date()));
+            parameter.put("date",dateFormat.format(new Date()));
             patientVisit=jdbcTemplate.query(getPatientVisitSql,parameter,new PatientVisitMapper());
 
 
